215fa3007d8669d1923631abe6532b68ed128217
Andrei Zhabinski <andrei.zhabinski@gmail.com>
2019-07-15 01:48:56 +0300

    Add code to test similarity of 2 images

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
+import dill
 from pathlib import Path
 import torch
 from torch import nn
 from torch.nn import functional as F
 from torch.nn.modules.distance import CosineSimilarity
 from torchvision import models
+from torchvision.datasets import ImageFolder
+from torchvision import transforms
 from datasets import TripletImageFolder
 from utils import imshow, train_test_loader
 from trainer2 import train
@@ -50,6 +53,7 @@ def main_old():
 
 
 FOOD101_IMG_PATH = Path("~/data/food-101/images").expanduser()
+MODEL_PATH = Path("~/models/foodie_e86.pkl").expanduser()
 
 
 def try_untrained():
@@ -68,3 +72,32 @@ def main():
     model = TripletNet(EmbeddingResnet()).to(device)
     loss_function = TripletLoss(1.0)
     train(model, trainset, testset, loss_function)
+    MODEL_PATH.parents[0].mkdirs(exist_ok=True)
+    torch.save(model, MODEL_PATH, pickle_module=dill)
+
+
+def similarity(model, dataset, i, j):
+    sim = CosineSimilarity()
+    a_img = dataset[i][0]
+    b_img = dataset[j][0]
+    imshow(a_img)
+    imshow(b_img)
+    a = model.get_embedding(a_img.unsqueeze(0).to(device))
+    b = model.get_embedding(b_img.unsqueeze(0).to(device))
+    print(f"sim(a,b) = {sim(a, b).item()}")
+    
+    
+def try_trained():
+    # dataset = TripletImageFolder(FOOD101_IMG_PATH)
+    dataset = ImageFolder(FOOD101_IMG_PATH, transform=transforms.Compose([
+            transforms.Resize((512, 512)),
+            transforms.ToTensor(),
+            transforms.Normalize((0.486, 0.459, 0.408), (0.229, 0.224, 0.225))
+        ]))
+    model = torch.load(MODEL_PATH, pickle_module=dill)
+    sim = CosineSimilarity()
+    a, b, c = map(lambda x: x.unsqueeze(0).to(device), [dataset[95000][0], dataset[93003][0], dataset[93004][0]])
+    a, b, c = map(model.get_embedding, (a, b, c))
+    print(f"sim(a,b) = {sim(a, b).item()}")
+    print(f"sim(a,c) = {sim(a, c).item()}")
+    print(f"sim(b,c) = {sim(b, c).item()}")
